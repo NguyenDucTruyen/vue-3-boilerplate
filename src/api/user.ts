aa81a25b44e79b0a92424900b337e200ee76b7f7
TruyenBD <mrtruyenbd1407@gmail.com>
2024-11-15 13:16:40 +0700

    feat: layout default

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,7 +1,13 @@
 import { $get } from '@/api/axios'
 
 export async function fetchUserData() {
-  return { data: true }
+  return {
+    data: {
+      name: 'shadcn',
+      email: 'm@example.com',
+      avatar: '/avatars/shadcn.jpg',
+    },
+  }
   // return await Promise.reject(new Error('Not implemented'))
 
   return $get('/user')
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,34 +1,21 @@
-<script setup lang="ts">
+<script setup>
 </script>
 
 <template>
-  <div>
-    <header class="header">
-      <h1>My Default Layout</h1>
-    </header>
-    <main>
-      <router-view />
-    </main>
-    <footer>
-      <p>© 2023 My Company</p>
+  <div class="flex flex-col h-full justify-between">
+    <div class="flex w-full lg:[height:calc(100%-5.75rem)] h-[calc(100%-10rem)]">
+      <AppSideBar class="max-lg:hidden" />
+      <div class="flex flex-col h-full w-full lg:[width:calc(100%-15rem)]">
+        <header class="flex h-16 items-center">
+          <AppHeader />
+        </header>
+        <ScrollArea class="flex flex-1 flex-col gap-4 p-4 bg-muted">
+          <router-view />
+        </ScrollArea>
+      </div>
+    </div>
+    <footer class="w-full h-40 lg:h-[5.75rem] ">
+      <!-- <AppFooter /> -->
     </footer>
   </div>
 </template>
-
-<style scoped>
-.header {
-  @apply bg-background p-4;
-}
-
-main {
-    padding: 1rem;
-}
-
-footer {
-    padding: 1rem;
-    text-align: center;
-    position: fixed;
-    width: 100%;
-    bottom: 0;
-}
-</style>
