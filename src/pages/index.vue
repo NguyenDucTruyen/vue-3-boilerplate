a22e63c7ba15ab0af327f40183bab2be2e9d481b
TruyenBD <mrtruyenbd1407@gmail.com>
2025-07-13 16:24:41 +0700

    feat: add confirmation modals to the home page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-import { showDefault, showError, showInfo, showSuccess, showWarning } from '@/utils/toast'
+import { useConfirmStore } from '@/stores/confirm'
+import { showError, showInfo, showSuccess, showWarning } from '@/utils/toast'
+
+const confirmStore = useConfirmStore()
 </script>
 
 <template>
@@ -25,6 +28,30 @@ import { showDefault, showError, showInfo, showSuccess, showWarning } from '@/ut
       </Button>
       <Button class="px-2 py-1 text-white rounded" @click="showInfo('info message!')">
         Show Toast info
+      </Button>
+    </div>
+    <div class="flex gap-4">
+      <Button
+        class="px-2 py-1 text-white rounded"
+        @click="confirmStore.showConfirmDialog({
+          title: 'Confirm Action',
+          message: 'Are you sure you want to proceed?',
+          confirmText: 'Yes',
+          cancelText: 'No',
+        })"
+      >
+        Show Confirmation Modal
+      </Button>
+      <Button
+        class="px-2 py-1 text-white rounded"
+        @click="confirmStore.showConfirmDialog({
+          title: 'Delete Item',
+          message: 'Are you sure you want to delete this item?',
+          confirmText: 'Delete',
+          cancelText: 'Cancel',
+        })"
+      >
+        Show Confirmation Modal with Delete Action
       </Button>
     </div>
     <ColorSelect />
